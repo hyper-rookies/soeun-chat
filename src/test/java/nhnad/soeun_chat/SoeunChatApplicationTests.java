bf31e328fc8549be0613a2abeb03d163e0500c46
saokiritori <soeun-lee@nhnad.com>
2026-02-25 09:48:17 +0900

    [init] Spring 환경 초기 세팅

---
+++ src/test/java/nhnad/soeun_chat/SoeunChatApplicationTests.java
@@ -0,0 +1,13 @@
+package nhnad.soeun_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoeunChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
