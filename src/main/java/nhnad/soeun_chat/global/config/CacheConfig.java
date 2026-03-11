2f8b87e6ef0aa202c85d2e916917a4f41fbc9fb8
saokiritori <soeun-lee@nhnad.com>
2026-03-11 09:32:02 +0900

    [refactor] caffeine -> Elasticache Redis 전환

--- src/main/java/nhnad/soeun_chat/global/config/CacheConfig.java
+++ src/main/java/nhnad/soeun_chat/global/config/CacheConfig.java
@@ -1,24 +1,26 @@
 package nhnad.soeun_chat.global.config;
 
-import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 
 @EnableCaching
 @Configuration
 public class CacheConfig {
 
     @Bean
-    public CacheManager cacheManager() {
-        CaffeineCacheManager manager = new CaffeineCacheManager("dashboard");
-        manager.setCaffeine(Caffeine.newBuilder()
-                .expireAfterWrite(1, TimeUnit.HOURS)
-                .maximumSize(10));
-        return manager;
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration dashboardConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofHours(1));
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .withCacheConfiguration("dashboard", dashboardConfig)
+                .build();
     }
 }
